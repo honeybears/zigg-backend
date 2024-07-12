73da0c69a915a40b75f583f8dc49f4d13800c53a
honeybears <clone0922@gmail.com>
2024-07-12 16:49:07 +0900

    Fix : Git action

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,8 +7,5 @@ JAR_FILE=$(ls -t $PROJECT_ROOT/build/libs/*.jar | head -n 1)
 unzip zigg.zip -d /home/ubuntu
 
 # 2. 빌드 (필요한 경우)
-chmod +x $PROJECT_ROOT/gradlew
-
-$PROJECT_ROOT/gradlew build
 
 java -jar $JAR_FILE -Dspring.profiles.active=dev
