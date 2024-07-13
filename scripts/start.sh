e06fc4f0cf0e04a038c6563bda705fb0dd13970f
honeybears <clone0922@gmail.com>
2024-07-13 12:01:18 +0900

    Fix : CI pipeline

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/zigg"
-JAR_FILE=$(ls -t $PROJECT_ROOT/build/libs/*.jar | head -n 1)
+JAR_FILE=$(ls -t "$PROJECT_ROOT"/build/libs/*.jar | head -n 1)
 
-# 1. 압축 해제
-unzip zigg.zip -d /home/ubuntu
+# 1. 압축 해제 (필요한 경우)
+if [ ! -d "$PROJECT_ROOT" ]; then
+  unzip zigg.zip -d /home/ubuntu
+fi
 
 # 2. 빌드 (필요한 경우)
+cd PROJECT_ROOT
+./gradlew build
 
-java -jar $JAR_FILE -Dspring.profiles.active=dev
+
+# 3. 실행
+java -jar "$JAR_FILE" --spring.profiles.active=dev
