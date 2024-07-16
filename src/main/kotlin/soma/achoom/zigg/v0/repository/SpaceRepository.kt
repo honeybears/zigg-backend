ce5110bd83aa1940e183a6b77461ae53f5325786
honeybears <clone0922@gmail.com>
2024-07-17 00:25:16 +0900

    Feat : SpaceRepository

--- src/main/kotlin/soma/achoom/zigg/v0/repository/SpaceRepository.kt
+++ src/main/kotlin/soma/achoom/zigg/v0/repository/SpaceRepository.kt
@@ -2,7 +2,8 @@ package soma.achoom.zigg.v0.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import soma.achoom.zigg.v0.model.Space
+import soma.achoom.zigg.v0.model.SpaceUser
 
 interface SpaceRepository : JpaRepository<Space, Long> {
-
+    fun findSpacesBySpaceUsersContaining(spaceUsers: SpaceUser): List<Space>
 }
