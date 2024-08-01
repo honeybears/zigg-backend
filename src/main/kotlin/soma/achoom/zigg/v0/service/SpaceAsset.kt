e15583d41c350a66af5cf2a1cced1f33dddb1a31
honeybears <clone0922@gmail.com>
2024-08-01 19:00:19 +0900

    Feat : history service

--- src/main/kotlin/soma/achoom/zigg/v0/service/SpaceAsset.kt
+++ src/main/kotlin/soma/achoom/zigg/v0/service/SpaceAsset.kt
@@ -7,7 +7,7 @@ import soma.achoom.zigg.v0.model.User
 abstract class SpaceAsset():BaseService() {
     protected fun validateSpaceUser(user: User, space:Space):SpaceUser{
         space.spaceUsers.forEach{
-            if(it.user.equals(user)){
+            if(it.user == user){
                 return it
             }
         }
