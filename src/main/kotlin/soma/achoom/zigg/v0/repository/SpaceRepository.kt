997ffaefd0fd07b19755897ec8032c14be850253
honeybears <clone0922@gmail.com>
2024-08-03 13:10:52 +0900

    Refactor : History service - 필요 없는 변수 제거 , 스페이스 내 권한 검증 코드 추가

--- src/main/kotlin/soma/achoom/zigg/v0/repository/SpaceRepository.kt
+++ src/main/kotlin/soma/achoom/zigg/v0/repository/SpaceRepository.kt
@@ -6,6 +6,7 @@ import soma.achoom.zigg.v0.model.Space
 import soma.achoom.zigg.v0.model.SpaceUser
 
 interface SpaceRepository : JpaRepository<Space, Long> {
+
     fun findSpacesBySpaceUsersContaining(spaceUsers: SpaceUser): List<Space>
     fun findSpaceBySpaceId(spaceId: Long): Space?
     fun findSpaceByHistoriesContains(history: History): Space?
