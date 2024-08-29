4bd971e458d5db6aa1cca4e7d25c450036e24993
강지훈 <clone0922@gmail.com>
2024-08-29 20:53:21 +0900

    Feat : space presignedurl

--- src/main/kotlin/soma/achoom/zigg/space/controller/SpaceController.kt
+++ src/main/kotlin/soma/achoom/zigg/space/controller/SpaceController.kt
@@ -1,12 +1,12 @@
 package soma.achoom.zigg.space.controller
 
-import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.multipart.MultipartFile
+import soma.achoom.zigg.history.dto.UploadContentTypeRequestDto
+import soma.achoom.zigg.s3.service.S3DataType
+import soma.achoom.zigg.s3.service.S3Service
 import soma.achoom.zigg.space.dto.SpaceReferenceUrlRequestDto
 import soma.achoom.zigg.space.dto.SpaceRequestDto
 import soma.achoom.zigg.space.dto.SpaceResponseDto
@@ -17,7 +17,13 @@ import java.util.*
 @RequestMapping("/api/v0/spaces")
 class SpaceController @Autowired constructor(
     private val spaceService: SpaceService,
+    private val s3Service: S3Service,
 ) {
+    @PostMapping("/pre-signed-url")
+    fun getPreSignUrl(@RequestBody uploadContentTypeRequestDto: UploadContentTypeRequestDto) : ResponseEntity<String> {
+        val preSignUrl = s3Service.getPreSignedPutUrl(S3DataType.SPACE_IMAGE,UUID.randomUUID(),uploadContentTypeRequestDto)
+        return ResponseEntity.ok(preSignUrl)
+    }
 
     @GetMapping
     fun getSpaces(authentication:Authentication) : ResponseEntity<List<SpaceResponseDto>> {
