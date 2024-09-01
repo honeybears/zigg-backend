77a501cb5760eae0d7e4f0b0c82c1bda48b2f272
강지훈 <clone0922@gmail.com>
2024-09-01 20:37:15 +0900

    Fix : Controller

--- src/main/kotlin/soma/achoom/zigg/firebase/controller/FCMController.kt
+++ src/main/kotlin/soma/achoom/zigg/firebase/controller/FCMController.kt
@@ -3,6 +3,7 @@ package soma.achoom.zigg.firebase.controller
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import soma.achoom.zigg.firebase.dto.FCMTokenRequestDto
@@ -14,7 +15,7 @@ class FCMController(
     private val fcmService: FCMService
 ) {
     @PostMapping("/token")
-    fun registerToken(authentication: Authentication,token: FCMTokenRequestDto) : ResponseEntity<Void> {
+    fun registerToken(authentication: Authentication,@RequestBody token: FCMTokenRequestDto) : ResponseEntity<Void> {
         fcmService.registerToken(authentication,token)
         return ResponseEntity.ok().build()
     }
