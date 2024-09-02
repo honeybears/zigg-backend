7266a35ed50d5ef2997c1676223bcff55f9e2d72
강지훈 <clone0922@gmail.com>
2024-09-02 17:19:42 +0900

    Update : FCM service layer 수정

--- src/main/kotlin/soma/achoom/zigg/user/controller/UserController.kt
+++ src/main/kotlin/soma/achoom/zigg/user/controller/UserController.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController
 import soma.achoom.zigg.auth.dto.OAuth2MetaDataRequestDto
 import soma.achoom.zigg.auth.dto.OAuth2UserRequestDto
 import soma.achoom.zigg.auth.service.AuthenticationService
+import soma.achoom.zigg.firebase.dto.FCMTokenRequestDto
+import soma.achoom.zigg.firebase.service.FCMService
 import soma.achoom.zigg.user.dto.UserExistsResponseDto
 import soma.achoom.zigg.user.dto.UserRequestDto
 import soma.achoom.zigg.user.dto.UserResponseDto
@@ -24,13 +27,23 @@ import soma.achoom.zigg.user.service.UserService
 @RequestMapping("/api/v0/users")
 class UserController @Autowired constructor(
     private val userService: UserService,
-
     private val authenticationService: AuthenticationService
 ) {
+    @PostMapping("/fcm")
+    fun registerToken(authentication: Authentication,@RequestBody token: FCMTokenRequestDto) : ResponseEntity<Void> {
+        userService.registerToken(authentication,token)
+        return ResponseEntity.ok().build()
+    }
     @GetMapping("/search/{nickname}")
     fun searchUser(authentication: Authentication,@PathVariable nickname:String): ResponseEntity<MutableSet<UserResponseDto>> {
         val userResponseDto = userService.searchUser(authentication,nickname)
         return ResponseEntity.ok(userResponseDto)
+    }
+
+    @PostMapping("/exists")
+    fun checkUserExists(@RequestBody oAuth2MetaDataRequestDto: OAuth2MetaDataRequestDto): ResponseEntity<UserExistsResponseDto> {
+        val userExistsResponseDto = authenticationService.userExistsCheckByOAuthPlatformAndProviderId(oAuth2MetaDataRequestDto)
+        return ResponseEntity.ok(userExistsResponseDto)
     }
     @PostMapping
     fun registerUser(@RequestBody oAuth2UserRequestDto: OAuth2UserRequestDto): ResponseEntity<Void> {
@@ -49,10 +62,14 @@ class UserController @Autowired constructor(
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDto)
     }
 
-
-    @PostMapping("/exists")
-    fun checkUserExists(@RequestBody oAuth2MetaDataRequestDto: OAuth2MetaDataRequestDto): ResponseEntity<UserExistsResponseDto> {
-        val userExistsResponseDto = authenticationService.userExistsCheckByOAuthPlatformAndProviderId(oAuth2MetaDataRequestDto)
-        return ResponseEntity.ok(userExistsResponseDto)
+    @DeleteMapping
+    fun deleteUser(authentication: Authentication):ResponseEntity<Void>{
+        userService.deleteUser(authentication)
+        return ResponseEntity.ok().build()
+    }
+    @DeleteMapping("/logout")
+    fun logout(authentication: Authentication): ResponseEntity<Void> {
+        userService.logoutUser(authentication)
+        return ResponseEntity.ok().build()
     }
 }
