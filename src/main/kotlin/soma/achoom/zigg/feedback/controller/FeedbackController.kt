e416843100a74e8d174fa472b589f918ae508bc1
강지훈 <clone0922@gmail.com>
2024-09-15 21:01:07 +0900

    Refactor : Controller create responseDto

--- src/main/kotlin/soma/achoom/zigg/feedback/controller/FeedbackController.kt
+++ src/main/kotlin/soma/achoom/zigg/feedback/controller/FeedbackController.kt
@@ -1,9 +1,6 @@
 package soma.achoom.zigg.feedback.controller
 
 import jakarta.validation.Valid
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
@@ -11,32 +8,61 @@ import org.springframework.web.bind.annotation.*
 import soma.achoom.zigg.feedback.dto.FeedbackRequestDto
 import soma.achoom.zigg.feedback.dto.FeedbackResponseDto
 import soma.achoom.zigg.feedback.service.FeedbackService
+import soma.achoom.zigg.global.ResponseDtoManager
 import java.util.UUID
 
 @RestController
 @RequestMapping("/api/v0/spaces/histories/feedbacks")
-class FeedbackController @Autowired constructor(private val feedbackService: FeedbackService) {
+class FeedbackController @Autowired constructor(
+    private val feedbackService: FeedbackService,
+    private val responseDtoManager: ResponseDtoManager
+) {
 
     @GetMapping("{spaceId}/{historyId}")
-    fun getFeedbacks(authentication: Authentication, @PathVariable spaceId: UUID, @PathVariable historyId: UUID): ResponseEntity<List<FeedbackResponseDto>> {
-        val feedbackResponseDto = feedbackService.getFeedbacks(authentication, spaceId, historyId)
-        return ResponseEntity.ok().body(feedbackResponseDto)
+    fun getFeedbacks(
+        authentication: Authentication,
+        @PathVariable spaceId: UUID,
+        @PathVariable historyId: UUID
+    ): ResponseEntity<List<FeedbackResponseDto>> {
+        val feedbacks = feedbackService.getFeedbacks(authentication, spaceId, historyId)
+        return ResponseEntity.ok().body(
+            feedbacks.map {
+                responseDtoManager.generateFeedbackResponseDto(it)
+            }
+        )
     }
 
     @PostMapping("{spaceId}/{historyId}")
-    fun createFeedback(authentication: Authentication, @PathVariable spaceId: UUID, @PathVariable historyId: UUID, @Valid @RequestBody feedbackRequestDto: FeedbackRequestDto): ResponseEntity<FeedbackResponseDto> {
-        val feedbackResponseDto = feedbackService.createFeedback(authentication, spaceId, historyId, feedbackRequestDto)
-        return ResponseEntity.ok().body(feedbackResponseDto)
+    fun createFeedback(
+        authentication: Authentication,
+        @PathVariable spaceId: UUID,
+        @PathVariable historyId: UUID,
+        @Valid @RequestBody feedbackRequestDto: FeedbackRequestDto
+    ): ResponseEntity<FeedbackResponseDto> {
+        val feedback = feedbackService.createFeedback(authentication, spaceId, historyId, feedbackRequestDto)
+        return ResponseEntity.ok().body(responseDtoManager.generateFeedbackResponseDto(feedback))
     }
 
     @PatchMapping("{spaceId}/{historyId}/{feedbackId}")
-    fun updateFeedback(authentication: Authentication, @PathVariable spaceId: UUID, @PathVariable historyId: UUID, @PathVariable feedbackId: UUID, @Valid @RequestBody feedbackRequestDto: FeedbackRequestDto): ResponseEntity<FeedbackResponseDto> {
-        val feedbackResponseDto = feedbackService.updateFeedback(authentication, spaceId, historyId, feedbackId, feedbackRequestDto)
-        return ResponseEntity.ok().body(feedbackResponseDto)
+    fun updateFeedback(
+        authentication: Authentication,
+        @PathVariable spaceId: UUID,
+        @PathVariable historyId: UUID,
+        @PathVariable feedbackId: UUID,
+        @Valid @RequestBody feedbackRequestDto: FeedbackRequestDto
+    ): ResponseEntity<FeedbackResponseDto> {
+        val feedback =
+            feedbackService.updateFeedback(authentication, spaceId, historyId, feedbackId, feedbackRequestDto)
+        return ResponseEntity.ok().body(responseDtoManager.generateFeedbackResponseDto(feedback))
     }
 
     @DeleteMapping("{spaceId}/{historyId}/{feedbackId}")
-    fun deleteFeedback(authentication: Authentication, @PathVariable spaceId: UUID, @PathVariable historyId: UUID, @PathVariable feedbackId: UUID): ResponseEntity<Any> {
+    fun deleteFeedback(
+        authentication: Authentication,
+        @PathVariable spaceId: UUID,
+        @PathVariable historyId: UUID,
+        @PathVariable feedbackId: UUID
+    ): ResponseEntity<Any> {
         feedbackService.deleteFeedback(authentication, spaceId, historyId, feedbackId)
         return ResponseEntity.noContent().build()
     }
