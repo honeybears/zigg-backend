155c85fb111e559496390b687dc9a83b05ea5648
강지훈 <clone0922@gmail.com>
2024-09-24 15:15:49 +0900

    Fix : Error with val

--- src/main/kotlin/soma/achoom/zigg/s3/service/S3DataType.kt
+++ src/main/kotlin/soma/achoom/zigg/s3/service/S3DataType.kt
@@ -4,9 +4,10 @@ enum class S3DataType(val path:String) {
     HISTORY_THUMBNAIL("thumbnail/history/"),
     HISTORY_VIDEO("video/history/"),
     SPACE_IMAGE("image/space/"),
-    SPACE_REFERENCE_VIDEO("reference/"),
     USER_BANNER_IMAGE("banner/"),
-    USER_PROFILE_IMAGE("profile/");
+    USER_PROFILE_IMAGE("profile/"),
+    POST_IMAGE("image/post/"),
+    POST_VIDEO("video/post/");
 
     fun getBucketName():String{
         return path
