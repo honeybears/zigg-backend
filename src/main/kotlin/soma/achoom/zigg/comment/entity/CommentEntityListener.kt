fa66071661cbcdc1c9d227e39c18e8ec32d5d67f
강지훈 <clone0922@gmail.com>
2024-11-04 16:27:10 +0900

    Test : Comment Service Test

--- src/main/kotlin/soma/achoom/zigg/comment/entity/CommentEntityListener.kt
+++ src/main/kotlin/soma/achoom/zigg/comment/entity/CommentEntityListener.kt
@@ -1,10 +1,17 @@
 package soma.achoom.zigg.comment.entity
 
 import jakarta.persistence.PrePersist
+import javax.annotation.PreDestroy
 
 class CommentEntityListener {
     @PrePersist
     fun prePersist(comment: Comment) {
         comment.parentComment?.replies?.add(comment)
+        comment.post.commentCnt++
+    }
+    @PreDestroy
+    fun preDestroy(comment: Comment) {
+        comment.parentComment?.replies?.remove(comment)
+        comment.post.commentCnt--
     }
 }
